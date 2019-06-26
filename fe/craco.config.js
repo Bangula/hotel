2b0b7eb191b84b05f126b6ceec1a07a12595d768
Dalibor Petric <dalibor.petric@quantox.com>
2019-06-26 15:19:21 +0200

    Initial setup finished

---
+++ fe/craco.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@actions": path.resolve(__dirname, "src/store/actions"),
+      "@helpers": path.resolve(__dirname, "src/helpers")
+    }
+  }
+};
