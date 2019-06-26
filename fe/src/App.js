07685bf4ef0383b8b13fdbe38e2124496546808c
Dalibor Petric <dalibor.petric@quantox.com>
2019-06-26 16:46:40 +0200

    home

--- fe/src/App.js
+++ fe/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 // Components
 import Header from "@components/layout/header/Header";
 import Rooms from "@components/rooms/Rooms";
+import Home from "@components/home/Home";
 
 function App() {
   return (
