2f56642ed9f2770457b42f20949b6ae4ffc6bff1
Dalibor Petric <dalibor.petric@quantox.com>
2019-06-26 16:45:28 +0200

    Added router

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./assets/styles/main.scss";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // Components
 import Header from "@components/layout/header/Header";
+import Rooms from "@components/rooms/Rooms";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Switch>
+          <Route to="/" component={Home} />
+          <Route to="/rooms" component={Rooms} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
