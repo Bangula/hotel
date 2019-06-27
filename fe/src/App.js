8214875fb36e458008007e89fc4b0a70e3018e01
Dalibor Petric <dalibor.petric@quantox.com>
2019-06-27 11:53:55 +0200

    Adder router links

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,7 +6,14 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "@components/layout/header/Header";
 import Rooms from "@components/rooms/Rooms";
 import Home from "@components/home/Home";
-
+import Booking from "@components/booking/Booking";
+import Events from "@components/events/Events";
+import Services from "@components/services/Services";
+import Gallery from "@components/gallery/Gallery";
+import Reviews from "@components/reviews/Reviews";
+import ContactUs from "@components/contactUs/ContactUs";
+import Login from "@components/login/Login";
+import Register from "@components/register/Register";
 function App() {
   return (
     <BrowserRouter>
@@ -14,7 +21,15 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route path="/booking" component={Booking} />
           <Route path="/rooms" component={Rooms} />
+          <Route path="/events" component={Events} />
+          <Route path="/services" component={Services} />
+          <Route path="/gallery" component={Gallery} />
+          <Route path="/reviews" component={Reviews} />
+          <Route path="/contact-us" component={ContactUs} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ fe/src/components/contactUs/ContactUs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactUs = () => {
+  return <div>CONTACT US</div>;
+};
+
+export default ContactUs;
