87b44063c1a20539b0a6b0ea3f9bcdbf3bbfd5b9
Dalibor Petric <dalibor.petric@quantox.com>
2019-07-05 16:51:42 +0200

    Dropzone added

--- fe/src/components/admin/gallery/CreateGallery.js
+++ fe/src/components/admin/gallery/CreateGallery.js
@@ -51,52 +51,60 @@ const Gallery = props => {
       <div
         {...getRootProps({
           className:
-            "h-24 bg-gray-200 mb-8 border-4 border-gray-400 border-dashed rounded "
+            "h-24 bg-gray-200 mb-8 border-4 border-gray-400 border-dashed rounded flex justify-center items-center"
         })}
       >
-        <input {...getInputProps()} className="" />
+        <input {...getInputProps()} classNamgit ae="" />
         <p className="">
           Drag 'n' drop some files here, or click to select files
         </p>
       </div>
       <aside className="">
-        <Paper>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Img</TableCell>
-                <TableCell>Img Name</TableCell>
+        {files.length ? (
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Img</TableCell>
+                  <TableCell>Img Name</TableCell>
 
-                <TableCell align="left">Delete Img</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {files.length
-                ? files.map((file, index) => (
-                    <TableRow key={file.path}>
-                      <TableCell component="th" scope="row">
-                        <img src={file.preview} className="h-8" />
-                      </TableCell>
-                      <TableCell component="th" scope="row">
-                        {file.path} - {file.size} bytes
-                      </TableCell>
+                  <TableCell align="right">Delete Img</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {files.length
+                  ? files.map((file, index) => (
+                      <TableRow key={file.path}>
+                        <TableCell component="th" scope="row">
+                          <img src={file.preview} className="h-8" />
+                        </TableCell>
+                        <TableCell component="th" scope="row">
+                          {file.path} - {file.size} bytes
+                        </TableCell>
 
-                      <TableCell align="left">
-                        {" "}
-                        <Button
-                          // onClick={() => }
-                          variant="contained"
-                          color="secondary"
-                        >
-                          Remove
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))
-                : null}
-            </TableBody>
-          </Table>
-        </Paper>
+                        <TableCell align="right">
+                          {" "}
+                          <Button
+                            onClick={() =>
+                              setFiles(
+                                files.filter(
+                                  (item, indexItem) => indexItem !== index
+                                )
+                              )
+                            }
+                            variant="contained"
+                            color="secondary"
+                          >
+                            Remove
+                          </Button>
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  : null}
+              </TableBody>
+            </Table>
+          </Paper>
+        ) : null}
       </aside>
     </section>
   );
